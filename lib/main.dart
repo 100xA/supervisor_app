57e43be5f8b468b0fce23055b786624ff5e9e2b3
JanAppAkademie <jan.biernacki@app-akademie.com>
2024-09-27 17:31:10 +0200

    Add Firebase Config 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:supervisor_app/home.dart';
+import 'package:supervisor_app/firebase_options.dart';
+import 'package:supervisor_app/presentation/home/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
---
+++ lib/presentation/home/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Center(
+              child: Text(
+                'Willkommen in der Betreuer-App',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Ich bin ein Betreuer"),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Ich bin Student"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
